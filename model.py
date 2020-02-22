e28ac8be7030096915c89eb667b981131bbe8fbf
rahulhasija05 <59260831+rahulhasija05@users.noreply.github.com>
2020-02-22 16:50:02 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -4,29 +4,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-dataset = pd.read_csv('hiring.csv')
+dataset = pd.read_csv('IRIS.csv')
 
-dataset['experience'].fillna(0, inplace=True)
+dataset['sepal.length'].fillna(0, inplace=True)
 
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
+dataset['sepal.width'].fillna(0, inplace=True)
+
+dataset['petal.length'].fillna(0, inplace=True)
+
+dataset['petal.width'].fillna(0, inplace=True)
 
 X = dataset.iloc[:, :3]
-
-#Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
 y = dataset.iloc[:, -1]
 
 #Splitting Training and Test Set
 #Since we have a very small dataset, we will train our model with all availabe data.
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+from sklearn.linear_model import LogisticRegression
+regressor = LogisticRegression()
 
 #Fitting model with trainig data
 regressor.fit(X, y)
